9ecfbe54436ab47ad4a74d2c1a83be7986c5bb7a
Lucas Tucker <tucker.lucas.1404@gmail.com>
2023-12-02 16:31:15 +0000

    Level cleanup. New functions and refactoring/renaming.

--- Pac-Man/src/Game/Level.java
+++ Pac-Man/src/Game/Level.java
@@ -41,69 +41,7 @@ public class Level
 			
 			int pixels[] = map.getRGB(0, 0, gameWidth, gameHeight, null, 0, gameWidth);
 			
-			Wall.wallMatrix = new Wall[gameWidth][gameHeight];
-			
-			for(int x = 0; x < gameWidth; x++)
-			{
-				for(int y = 0; y < gameHeight; y++)
-				{
-					int color = pixels[x + (y * gameWidth)];
-					
-					switch(color)
-					{
-						case black:
-							
-							Wall.wallMatrix[x][y] = new Wall(x*32, y*32);
-							break;
-							
-						case gray:			
-							
-							SpawnBoxDoor.spawnBoxDoor.x = x*32;
-							SpawnBoxDoor.spawnBoxDoor.y = y*32;
-							break;
-							
-						case darkYellow:			
-							
-							Pacman.pacman.x = x*32;
-							Pacman.pacman.y = y*32;
-							break;
-						
-						case red:			
-							
-							Ghost.ghostArray[0].x = x*32;
-							Ghost.ghostArray[0].y = y*32;
-							break;
-							
-						case cyan:			
-							
-							Ghost.ghostArray[1].x = x*32;
-							Ghost.ghostArray[1].y = y*32;
-							break;
-							
-						case purple:
-							
-							Ghost.ghostArray[2].x = x*32;
-							Ghost.ghostArray[2].y = y*32;
-							break;
-							
-						case orange:
-							
-							Ghost.ghostArray[3].x = x*32;
-							Ghost.ghostArray[3].y = y*32;
-							break;
-							
-						case white:
-							
-							Food.foodList.add(new Pellet(x*32, y*32));
-							break;
-							
-						case lightYellow:
-							
-							Food.foodList.add(new Energizer(x*32, y*32));
-							break;
-					}
-				}	
-			}
+			loadElementsByColor(pixels);
 		} 
 		catch (IOException e) 
 		{
@@ -111,24 +49,98 @@ public class Level
 		}
 	}
 	
-	private static void drawData(Graphics g)
-	{							
-		Game.setLetteringStyle(g, Color.white, Font.DIALOG_INPUT, 23);
+	private void loadElementsByColor(int pixels[])
+	{
+		Wall.wallMatrix = new Wall[gameWidth][gameHeight];
 		
-		g.drawString("SCORE: ", 20, 745);	
-		g.drawString(String.valueOf(Game.score), 105, 745);
-		g.drawString("HIGHSCORE: ", 210, 745);	
-		g.drawString(String.valueOf(Game.highscore), 350, 745);
-		g.drawString("LIVES:", 470, 745);
-		
-		for(int i = 0; i < Pacman.numberOfLives; i++)
+		for(int x = 0; x < gameWidth; x++)
 		{
-			g.drawImage(Animation.alivePacmanSprites[0][2], 555 + (35 * i), 720, 32, 32, null);
+			for(int y = 0; y < gameHeight; y++)
+			{
+				int color = pixels[x + (y * gameWidth)];
+				
+				switch(color)
+				{
+					case black:
+						
+						Wall.wallMatrix[x][y] = new Wall(x*objectWidth, y*objectHeight);
+						break;
+						
+					case gray:			
+						
+						SpawnBoxDoor.spawnBoxDoor.x = x*objectWidth;
+						SpawnBoxDoor.spawnBoxDoor.y = y*objectHeight;
+						break;
+						
+					case darkYellow:			
+						
+						Pacman.pacman.x = x*objectWidth;
+						Pacman.pacman.y = y*objectHeight;
+						break;
+					
+					case red:			
+						
+						Ghost.ghostArray[0].x = x*objectWidth;
+						Ghost.ghostArray[0].y = y*objectHeight;
+						break;
+						
+					case cyan:			
+						
+						Ghost.ghostArray[1].x = x*objectWidth;
+						Ghost.ghostArray[1].y = y*objectHeight;
+						break;
+						
+					case purple:
+						
+						Ghost.ghostArray[2].x = x*objectWidth;
+						Ghost.ghostArray[2].y = y*objectHeight;
+						break;
+						
+					case orange:
+						
+						Ghost.ghostArray[3].x = x*objectWidth;
+						Ghost.ghostArray[3].y = y*objectHeight;
+						break;
+						
+					case white:
+						
+						Food.foodList.add(new Pellet(x*objectWidth, y*objectHeight));
+						break;
+						
+					case lightYellow:
+						
+						Food.foodList.add(new Energizer(x*objectWidth, y*objectHeight));
+						break;
+				}
+			}	
 		}
 	}
 	
+	
+	public void tick()
+	{
+		if(Food.foodList.size() == 0)
+		{
+			Game.gameStatus = Game.win;
+		}
+	}
+	
+	
 	public void render(Graphics g)
 	{
+		Pacman.pacman.render(g);
+		
+		if(Game.gameStatus != Game.lifeLost)
+		{
+			Ghost.ghostArray[0].render(g);
+			Ghost.ghostArray[1].render(g);
+			Ghost.ghostArray[2].render(g);
+			Ghost.ghostArray[3].render(g);
+		}
+
+		SpawnBoxDoor.spawnBoxDoor.render(g);
+		BonusScore.bonusScore.render(g);
+		
 		for(int x = 0; x < gameWidth; x++)
 		{
 			for(int y = 0; y < gameHeight; y++)
@@ -145,27 +157,22 @@ public class Level
 			Food.foodList.get(i).render(g);
 		}
 		
-		Pacman.pacman.render(g);
-		
-		if(Game.gameStatus != Game.lifeLost)
-		{
-			Ghost.ghostArray[0].render(g);
-			Ghost.ghostArray[1].render(g);
-			Ghost.ghostArray[2].render(g);
-			Ghost.ghostArray[3].render(g);
-		}
-
-		SpawnBoxDoor.spawnBoxDoor.render(g);
-		BonusScore.bonusScore.render(g);
-		
-		drawData(g);
+		displayGameStats(g);
 	}
 	
-	public void tick()
-	{
-		if(Food.foodList.size() == 0)
+	private static void displayGameStats(Graphics g)
+	{							
+		Game.setLetteringStyle(g, Color.white, Font.DIALOG_INPUT, 23);
+		
+		g.drawString("SCORE: ", 20, 745);	
+		g.drawString(String.valueOf(Game.score), 105, 745);
+		g.drawString("HIGHSCORE: ", 210, 745);	
+		g.drawString(String.valueOf(Game.highscore), 350, 745);
+		g.drawString("LIVES:", 470, 745);
+		
+		for(int i = 0; i < Pacman.numberOfLives; i++)
 		{
-			Game.gameStatus = Game.win;
+			g.drawImage(Animation.alivePacmanSprites[0][2], 555 + (35 * i), 720, objectWidth, objectHeight, null);
 		}
 	}
 }
